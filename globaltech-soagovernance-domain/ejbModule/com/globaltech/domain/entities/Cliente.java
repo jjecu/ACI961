660d3c3aaddb45d552f320e15d7b65e23f802ab5
Martin Mena <martinmena@outlook.com>
2013-06-02 22:12:19 -0500

    proyecto para la capa de dominio

---
+++ globaltech-soagovernance-domain/ejbModule/com/globaltech/domain/entities/Cliente.java
@@ -0,0 +1,60 @@
+package com.globaltech.domain.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Entity implementation class for Entity: Cliente
+ * 
+ */
+@Entity
+@Table(name = "cliente")
+public class Cliente implements Serializable {
+
+	@Id
+	private String cedula;
+	private String nombre;
+	private String apellido;
+	private String cargo;
+	private static final long serialVersionUID = 1L;
+
+	public Cliente() {
+		super();
+	}
+
+	public String getCedula() {
+		return this.cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return this.apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getCargo() {
+		return this.cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+}
